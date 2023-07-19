0cd54120523845f9b603352198388be3b56513a1
Diego de Souza Santana <79605718+Diego-Sant@users.noreply.github.com>
2023-07-19 17:10:30 -0300

    environment setup

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+const Dashboard = () => {
+    return (
+      <div className="text-6xl">Hello</div>
+    )
+}
+
+export default Dashboard;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,7 @@
+const Landing = () => {
+    return (
+      <div className="text-6xl">Landing</div>
+    )
+}
+
+export default Landing;
